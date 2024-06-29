abb1f1438178a68332c56c7714af5fa9088ef6fa
SolveNaN <dagoberto6993@gmail.com>
2024-06-29 01:08:18 -0500

    first commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,23 @@
+﻿using Inventario.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System.Net.Sockets;
+
+namespace Inventario.Server.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+        }
+
+
+        public DbSet<Producto> Productos { get; set; }
+
+
+    }
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,10 @@
+﻿namespace Inventario.Models
+{
+    public class Producto
+    {
+        public int Id { get; set; }
+        public string Nombre { get; set; } = string.Empty;
+        public double? Valor { get; set; }
+
+    }
+}
